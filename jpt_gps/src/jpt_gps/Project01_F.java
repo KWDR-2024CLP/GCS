d88a6ee9b3eeac59a19ec25d2ca3199d8fd32415
SeongGun <kimseonggun524@gmail.com>
2024-12-06 21:04:22 +0900

    Change current map, camera, function location in GCS

--- jpt_gps/src/jpt_gps/Project01_F.java
+++ jpt_gps/src/jpt_gps/Project01_F.java
@@ -1,7 +1,9 @@
 package jpt_gps;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,15 +20,37 @@ public class Project01_F {
         Container c = frm.getContentPane();
         c.setLayout(new BorderLayout());
 
-        // 오른쪽 패널(지도)
+        // 왼쪽 패널 (지도)
         imageLabel = new JLabel();
-        JPanel rightPanel = new JPanel(new BorderLayout());
-        rightPanel.add(imageLabel, BorderLayout.CENTER);
-        c.add(BorderLayout.EAST, rightPanel);
+        JPanel leftPanel = new JPanel(new BorderLayout());
+        leftPanel.add(imageLabel, BorderLayout.CENTER); // 지도 추가
+        leftPanel.setPreferredSize(new Dimension(750, 800));
+        c.add(BorderLayout.WEST, leftPanel);
 
-        // 왼쪽 패널(빈칸)
-        JPanel leftPanel = new JPanel();
-        c.add(BorderLayout.CENTER, leftPanel);
+        // 오른쪽 패널 (위 아래로 나뉨)
+        JPanel rightPanel = new JPanel(new BorderLayout());
+        rightPanel.setPreferredSize(new Dimension(750, 800));
+
+        // 상단 패널 (문구)
+        JPanel upperPanel = new JPanel(new BorderLayout());
+        upperPanel.setBackground(Color.LIGHT_GRAY);
+        upperPanel.setPreferredSize(new Dimension(750, 400));
+
+        JLabel droneLabel = new JLabel("드론 카메라 송출 예정입니다...", JLabel.CENTER); // 중앙 정렬
+        droneLabel.setForeground(Color.BLACK); // 글자 색 설정
+        droneLabel.setFont(droneLabel.getFont().deriveFont(20.0f)); // 글자 크기 설정
+        //upperPanel.add(droneLabel, BorderLayout.CENTER); // 상단 패널 중앙에 추가
+
+        // 하단 패널 (빈칸)
+        JPanel lowerPanel = new JPanel();
+        lowerPanel.setBackground(Color.WHITE);
+        lowerPanel.setPreferredSize(new Dimension(750, 400));
+
+        // 오른쪽 패널에 추가
+        rightPanel.add(upperPanel, BorderLayout.NORTH);
+        rightPanel.add(lowerPanel, BorderLayout.SOUTH);
+
+        c.add(BorderLayout.CENTER, rightPanel);
 
         // 네이버 지도 로드
         loadMap();
@@ -38,8 +62,8 @@ public class Project01_F {
         AddressVO vo = new AddressVO();
         vo.setX("128.467687"); // 기본 경도 (학교 기준)
         vo.setY("36.168547"); // 기본 위도 (학교 기준)
-        vo.setRoadAddress("현재 위치");
-        vo.setJibunAddress("현재 위치");
+        vo.setRoadAddress("NOW");
+        vo.setJibunAddress("NOW");
 
         // 지도 표시
         new NaverMap(this).map_service(vo);
